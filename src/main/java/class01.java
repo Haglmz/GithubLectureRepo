aacd14cf239266be363ec368bfd7d04825bfa38d
Hasan Ali Gülmez <avhasanaligulmez@gmail.com>
2023-08-19 21:15:54 +0300

    Versiyon Uc

--- src/main/java/class01.java
+++ src/main/java/class01.java
@@ -9,6 +9,8 @@ public class class01 {
         //Versiyon 2
         System.out.println("versiyon ikiyi olusturuyoruz");
 
+        //Verisyon 3
+        System.out.println("verisyon 3 devam");
 
 
     }
